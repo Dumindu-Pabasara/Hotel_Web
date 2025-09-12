33486bfd752703def94677d161e51d842d24f01f
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-09-12 12:45:23 +0530

    Create About_Us.jsx

---
+++ Hote_Website/src/Components/About Us/About_Us.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './About_Us.css'
+
+const About_Us = () => {
+  return (
+    <div className='about-us container'>
+        <h1>Welcome to EPITOME</h1>
+        <p>The venue is designed to embody the pinnacle 
+            of opulence and within its walls, you will 
+            discover the grandeur of Sri Lanka's finest 
+            and most expansive pillarless ballroom, 
+            capable of accommodating anywhere from 30 
+            to 2000 guests. Additionally, the hotel 
+            boasts a remarkable array of culinary 
+            delights, including a restaurant, coffee 
+            shop, and pool bar, where you can indulge 
+            your palate with a wide selection of 
+            delectable cuisine and beverages.</p>
+            <button className='btn2'>Learn More</button>
+    </div>
+  )
+}
+
+export default About_Us 
