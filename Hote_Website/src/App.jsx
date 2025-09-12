5ec366780f38011b84e52d2f547ae6029d1bbdcc
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-09-12 12:44:53 +0530

    Update App.jsx

--- Hote_Website/src/App.jsx
+++ Hote_Website/src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Nav_Bar from './Components/Nav Bar/Nav_Bar';
 import { Hero } from './Components/Hero/Hero';
 import About_Us from './Components/About Us/About_Us';
+import Accomodation from './Components/Accomodation/Accomodation';
+import { Experiance } from './Components/Experiance/Experiance';
 
 
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
       <Nav_Bar />
       <Hero />
       <About_Us />
+      <Accomodation />
+      <Experiance />
     </div>
   );
 }
