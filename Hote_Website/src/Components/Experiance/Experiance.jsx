687340d9bf8c4536bcfd4afd6aca3941a02c38a3
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-09-13 13:26:48 +0530

    Create Experiance.jsx

---
+++ Hote_Website/src/Components/Experiance/Experiance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Experiance.css'
+
+export const Experiance = () => {
+  return (
+    <div className="experiance">
+        <button className="explore-btn4">Amenilities</button>
+        <h1>Experiance Warmth and Wellness</h1>
+    </div>
+  )
+}
