f5ed60e70a3c6e94e8ada28ed0779c7070039057
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-09-13 13:26:25 +0530

    Create Hero.jsx

---
+++ Hote_Website/src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+
+export const Hero = () => {
+  return (
+    <div className='hero container'>
+        <div className="hero-text">
+            <h1>Relax</h1>
+            <button className='btn1'>Book Now</button>
+        </div>
+    </div>
+  )
+}
