8277542d5be78d4e2e41ed9eac8c96047b42cd81
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-10-01 22:21:01 +0530

    Update Nav_Bar.jsx

--- Hote_Website/src/Components/Nav Bar/Nav_Bar.jsx
+++ Hote_Website/src/Components/Nav Bar/Nav_Bar.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import './Nav_Bar.css'
-import logo from '../../Assets/logo.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav_Bar.css';
+import logo from '../../Assets/logo.png';
 
 const Nav_Bar = () => {
   return (
     <nav className='container'>
-        <img src={logo} alt="" className='logo'/>
-        <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Accommodation</li>
-            <li>Experiences</li>
-            <li>Gallery</li>
-            <li>Contact Us</li>
-            <li><button className='btn'>Book Now</button></li>
-        </ul>
-       
+      <img src={logo} alt='' className='logo' />
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About Us</Link></li>
+        <li><Link to='/accommodation'>Accommodation</Link></li>
+        <li><Link to='/experiences'>Experiences</Link></li>
+        <li><Link to='/gallery'>Gallery</Link></li>
+        <li><Link to='/booking'>Book Now</Link></li>
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default Nav_Bar
+export default Nav_Bar;
